3a0f0164f78d07cd5bc079d37e0fc9fa99cd5ab8
inkyant <80181074+inkyant@users.noreply.github.com>
2024-11-09 03:41:22 -0800

    add experimental text entry

--- client/src/components/user-interface/SubmitButton.tsx
+++ client/src/components/user-interface/SubmitButton.tsx
@@ -1,20 +1,33 @@
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import Button from './common/Button';
 import OrderEntryContext from '../context/OrderEntryContext';
 import { OrderEntryActionType } from '../../types/context/orderEntryAction';
 import WorldContext from '../context/WorldContext';
+import TextInput from './common/TextInput';
+import { parseOrder } from '../../types/str_to_orders';
+import Order from '../../types/order';
 
 const SubmitButton = () => {
   const { world, submitOrders, isLoading, error } = useContext(WorldContext);
   const { dispatch, orders } = useContext(OrderEntryContext);
+  const textRef = useRef<Order[] | null>(null)
 
   const onSubmit = () => {
     dispatch({ $type: OrderEntryActionType.Submit });
-    submitOrders(orders);
+    submitOrders(textRef.current ? textRef.current : orders);
   };
+
+  const onChange = (value: string) => {
+    const newOrders = value.split(";").map((str) => {
+      return parseOrder(str.split(":")[1], str.split(":")[0])
+    }).filter((value) => value != undefined)
+    if (newOrders)
+      textRef.current = newOrders
+  }
 
   return (
     <div className="absolute right-10 bottom-10">
+      {/* <TextInput placeholder='input moves' onChange={onChange}></TextInput> */}
       <Button
         text="Submit"
         onClick={onSubmit}
---
+++ client/src/types/str_to_orders.ts
@@ -0,0 +1,161 @@
+
+import Location from './location';
+import Phase from './enums/phase';
+import UnitType from './enums/unitType';
+import Order, { OrderStatus, OrderType } from './order';
+import Unit from './unit';
+import Nation from './enums/nation';
+
+export function parseOrder(orderString: string, country: string) {
+
+  const nation = Object.values(Nation).find(
+    nation => nation.toLowerCase() === country.toLowerCase()
+  );
+
+  if (!nation) return
+  orderString = orderString.trim()
+  const parts = orderString.split(' ');
+
+  if (parts.length < 5) {
+    return
+  }
+
+  // Parse common Location data
+  const timeline = parseInt(parts[0].substring(1));
+  const phase = parts[1].charAt(0);
+  const year = 1900 + parseInt(parts[1].substring(1));
+
+  const location: Location = {
+    timeline,
+    year,
+    phase: phase === 'S' ? Phase.Spring : phase === 'F' ? Phase.Fall : Phase.Winter,
+    region: ''
+  };
+
+  // For Build and Disband orders in Winter phase
+  if (phase === 'W') {
+    if (parts[2] === 'D') {
+      location.region = parts[4]
+      return parseDisband(parseUnit(nation, parts[3]), location);
+    }
+    location.region = parts[3]
+    return parseBuild(parseUnit(nation, parts[2]), location);
+  }
+
+  // For other orders, standard format
+  location.region = parts[3];
+  const unit = parseUnit(nation, parts[2])
+
+  if (parts[4] === "H") {
+    return parseHold(unit, location)
+  }
+
+  // Determine order type and parse accordingly
+  switch (parts[4]) {
+    case '->':
+      const dest = parseDestination(orderString.split("->")[1], location)
+      return parseMove(unit, location, dest);
+    case 'S':
+
+      const supportStr = orderString.split(" ").slice(5).join(" ")
+      const supportFrom = parseDestination(supportStr.split("->")[0], location)
+      const supportTo = parseDestination(supportStr.split("->")[1], location)
+
+      return parseSupport(unit, location, supportFrom, supportTo);
+    case 'C':
+      
+      const convoyStr = orderString.split(" ").slice(5).join(" ")
+      const convoyFrom = parseDestination(convoyStr.split("->")[0], location)
+      const convoyTo = parseDestination(convoyStr.split("->")[1], location)
+
+      return parseConvoy(unit, location, convoyFrom, convoyTo);
+    default:
+      throw new Error('Invalid order type');
+  }
+}
+
+function parseDestination(destination_str: string, default_location: Location): Location {
+  const destination_parts = destination_str.trim().split(" ")
+
+  const destination: Location = {
+    timeline: default_location.timeline,
+    year: default_location.year,
+    phase: default_location.phase,
+    region: destination_parts[destination_parts.length - 1]
+  }
+
+  if (destination_parts.length == 3) {
+    destination.timeline = parseInt(destination_parts[0].substring(1));
+    destination.phase = destination_parts[1].charAt(0) === "S" ? Phase.Spring : Phase.Fall;
+    destination.year = 1900 + parseInt(destination_parts[1].substring(1));      
+  }
+
+  return destination
+}
+
+function parseUnit(nation: Nation, type: string): Unit {
+  return {
+    owner: nation,
+    type: type === "F" ? UnitType.Fleet : UnitType.Army,
+    mustRetreat: false
+  }
+}
+
+function parseHold(unit: Unit, location: Location): Order {
+  return {
+    $type: OrderType.Hold,
+    status: OrderStatus.New,
+    unit,
+    location
+  };
+}
+
+function parseMove(unit: Unit, location: Location, destination: Location): Order {
+  return {
+    $type: OrderType.Move,
+    status: OrderStatus.New,
+    unit,
+    location,
+    destination: destination
+  };
+}
+
+function parseSupport(unit: Unit, location: Location, supportFrom: Location, supportTo: Location): Order {
+  return {
+    $type: OrderType.Support,
+    status: OrderStatus.New,
+    unit,
+    location,
+    supportLocation: supportFrom,
+    destination: supportTo
+  };
+}
+
+function parseConvoy(unit: Unit, location: Location, convoyFrom: Location, convoyTo: Location): Order {
+  return {
+    $type: OrderType.Convoy,
+    status: OrderStatus.New,
+    unit,
+    location,
+    convoyLocation: convoyFrom,
+    destination: convoyTo
+  };
+}
+
+function parseBuild(unit: Unit, location: Location): Order {
+  return {
+    $type: OrderType.Build,
+    status: OrderStatus.New,
+    unit: unit,
+    location: location
+  };
+}
+
+function parseDisband(unit: Unit, location: Location): Order {
+  return {
+    $type: OrderType.Disband,
+    status: OrderStatus.New,
+    unit: unit,
+    location: location
+  };
+}
